f564cc979df5ade9b607ee67057bda42624c6a8e
W-McDonald <westonjmcdonald@gmail.com>
2021-07-27 01:01:50 -0500

    creating a parent class for telegram bot dev

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,17 +1,18 @@
+#!/usr/bin/env python3
 import requests
 import json
-import configparser as cfg
+import configparser
 import sys
 sys.dont_write_bytecode = True
 
 
-class TelegramBot():
+class TelegramBot:
 
     def __init__(self, configfile):
         self.config = self.__parse_config(configfile)
-        self.name = config['name']
-        self.api_token = config['api_token']
-        self.base = "https://api.telegram.org/bot{}/".format(self.token)
+        self.name = self.config.get('DEFAULT', 'name')
+        self.api_token = self.config.get('DEFAULT', 'api_token')
+        self.base = "https://api.telegram.org/bot{}/".format(self.api_token)
         self.update_id = None
         self.message = None
         self.chat = None
@@ -21,17 +22,20 @@ class TelegramBot():
         self.message_text = None
         self.command = None
         self.response = None
+        self.cmds = ['/help']
 
-    def __parse_config(self, config):
-        parser = cfg.ConfigParser()
-        parsed_config = parser.read(config)
-        return parsed_config
+    def __parse_config(self, configfile):
+        config_parser = configparser.ConfigParser()
+        config_parser.read(configfile)
+        return config_parser
 
-    def send_message(self, chat_id):
-        url = self.base + "sendMessage?chat_id={}&text={}".format(chat_id, self.response)
+    def __build_url_and_send_request(self):
+        #TODO   think about how instead of sending a specific set of args in this url, just build
+        #TODO   every possible arg and then append them to the base url if they have a value assigned
+        url = self.base + "sendMessage?chat_id={}&text={}".format(self.chat_id, self.response)
         requests.get(url)
 
-    def parse_message(self, data):
+    def __parse_message(self, data):
         self.update_id = data['update_id']
         if 'message' in data.keys():
             self.message = data['message']
@@ -47,7 +51,7 @@ class TelegramBot():
 
     def respond(self):
         if self.response:
-            self.send_message(self.chat_id)
+            self.__build_url_and_send_request()
         self.response = None
 
     def get_updates(self, offset=None):
@@ -57,3 +61,5 @@ class TelegramBot():
         r = requests.get(url)
         return json.loads(r.content)
 
+    def speak(self):
+        print('hello')
